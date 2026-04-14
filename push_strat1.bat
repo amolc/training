1f4fcef4ada20f1ab717e3149ec45b93bf3daece
amolc <amolch001@gmail.com>
2026-04-14 13:22:00 +0530

    Add scripts as submodule from training repo

---
+++ push_strat1.bat
@@ -0,0 +1,5 @@
+cd stockmarket/vedant/scripts
+git add strat1.py
+git commit -m &quot;Add strat1.py&quot;
+git remote add origin https://github.com/vedantnambiar/strat1.git
+git push -u origin master
